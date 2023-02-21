cff97bdd849796d69144aca87e99caa551e30781
KiranMantha <kiranv.mantha@gmail.com>
2023-02-21 22:45:19 +0530

    added storybook for angular app

---
+++ .storybook/preview.js
@@ -0,0 +1,31 @@
+import {useTheme} from './useTheme';
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
+setCompodocJson(docJson);
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: { inlineStories: true },
+}
+
+export const globalTypes = {
+  theme: {
+    name: 'Toggle theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['light', 'dark'],
+      showName: true,
+      dynamicTitle: true
+    }
+  }
+}
+
+export const decorators = [useTheme];
